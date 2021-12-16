296000f5104d31105f5235afc4382ed241e671ce
EsAlco <90135660+EsAlco@users.noreply.github.com>
2021-12-16 01:28:28 +0100

    Initial Commit

---
+++ EscapeDiariesUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EscapeDiariesUI
+//
+//  Created by Esther Alcoceba Gutiérrez de León on 16/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EscapeDiariesUI/EscapeDiariesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeDiariesUIApp.swift
+//  EscapeDiariesUI
+//
+//  Created by Esther Alcoceba Gutiérrez de León on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct EscapeDiariesUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
